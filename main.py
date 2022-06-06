7edabfec0dc1aac5a750182d9d56e662bd886005
phong.dang <phong12a5@gmail.com>
2022-06-06 14:02:31 +0700

    download multi account

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 from tiktok_scraper import TikTokScraper
+import concurrent.futures
+import urllib.request
 
+def scrap(account : str):
+    scraper = TikTokScraper(account = account)
+    scraper.run()
 
 def main():
     print(" ******************** STARTED ********************")
 
-    scraper = TikTokScraper("doodi_zone")
-    scraper.run()
-    
+    # scraper = TikTokScraper(account = "ditesuna")
+    # scraper.run()
+
+    ACCOUNTS = ['ditesuna',
+            'anquynhtrang',
+            'kauut30',
+            'endrileka633',
+            'guoervictoria0212,
+            'rollerming',
+            'janice_yy',
+            'nyxinhlamne1212'
+            'o___o988',
+            'nana_omake1',
+            'herher408',
+            'ngoctuyen_09',
+            'pjlquzl6gpqr',
+            'haquy1996',
+            'ting728118']
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+            executor.map(scrap, ACCOUNTS)
+
     print(" ******************** DONE ********************")
 
 
